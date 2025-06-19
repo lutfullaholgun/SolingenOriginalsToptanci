8442e35e28b94a190129365c0a9eb783188355f3
lutfullaholgun <ltfllh99@gmail.com>
2025-06-19 10:34:53 +0300

    Add project files.

---
+++ SolingenOriginalsToptanci.Data/SolingenContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolingenOriginalsToptanci.Models.Entities;
+
+namespace SolingenOriginalsToptanci.Data
+{
+    public class SolingenContext : DbContext
+    {
+        public SolingenContext(DbContextOptions<SolingenContext> options)
+            : base(options)
+        {
+        }
+
+        // Buraya veritabanı tablolarına karşılık gelen DbSet'leri ekle
+        // Örnek:
+        // public DbSet<Product> Products { get; set; }
+        // public DbSet<Customer> Customers { get; set; }
+    }
+}
