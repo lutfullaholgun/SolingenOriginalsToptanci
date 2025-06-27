3f68df8155bcd2c0ba06e148e2dfac06a97ac9b7
lutfullaholgun <ltfllh99@gmail.com>
2025-06-27 17:25:34 +0300

    #27

--- SolingenOriginalsToptanci.Models/Entities/ApplicationUser.cs
+++ SolingenOriginalsToptanci.Models/Entities/ApplicationUser.cs
@@ -1,14 +1,6 @@
-﻿// SolingenOriginalsToptanci.Models.Entities/ApplicationUser.cs
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace SolingenOriginalsToptanci.Models.Entities
+public class ApplicationUser : IdentityUser
 {
-    // IdentityUser'dan türeyerek varsayılan kullanıcı alanlarına ekleme yapabilirsiniz.
-    // Şimdilik ek alan eklemiyoruz, sadece türetiyoruz.
-    public class ApplicationUser : IdentityUser
-    {
-        // Ek özellikler buraya eklenebilir, örneğin:
-        // public string FullName { get; set; }
-        // public DateTime DateOfBirth { get; set; }
-    }
+    // Gerekirse ekstra alanlar eklenebilir
 }
